65a9b3966d25444f33fed4542c86ff1cbce12473
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-13 09:18:46 -0500

    admin profile options added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from users.models import Profile
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "phone_number", "website", "picture", "biography")
+    list_editable = ("phone_number", "website", "picture", "biography")
+    search_fields = ("user__email", "user__first_name",
+                     "user__last_name", "phone_number", "user__username")
+    list_filter = ("created", "modified", "user__is_active", "user__is_staff")
