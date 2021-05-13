09cc2baa68bdba44cba51b9691a5d40b9693fb5b
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-13 13:51:02 -0500

    signup added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.models import Profile
+
+from django.db.utils import IntegrityError
 
 # Create your views here.
 
@@ -16,6 +20,55 @@ def login_view(request):
         else:
             return render(request, "users/login.html", {"error": "Invalid username or password"})
     return render(request, "users/login.html")
+
+
+def signup_view(request):
+    '''Signup view'''
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirmation']
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+
+        # PASSWORD VALIDATION
+        if password != password_confirm:
+            error = 'The passwords do not match.'
+            return render(request, 'users/signup.html', {'error': error})
+
+        # EMAIL VALIDATION
+        u = User.objects.filter(email=email)
+        if u:
+            error = f'There is another account using {email}'
+            return render(request, 'users/signup.html', {'error': error})
+
+        # USERNAME VALIDATION
+        try:
+            user = User.objects.create_user(
+                username=username, password=password)
+            user.email = email
+            user.first_name = first_name
+            user.last_name = last_name
+
+            user.save()
+
+            profile = Profile(user=user)
+            profile.save()
+
+            login(request, user)
+            # CAMBIAR >> Redireccionar a completar perfil
+            return redirect('profile')
+        except IntegrityError as ie:
+            error = f'There is another account using {usermame}'
+            return render(request, 'users/signup.html', {'error': error})
+
+    return render(request, 'users/signup.html')
+
+
+def profile_view(request):
+    return render(request, "users/profile.html")
 
 
 @login_required
