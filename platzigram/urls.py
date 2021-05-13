91dd7244914e2539aafc2ce5fd6d2afff07d985e
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-05-13 11:03:57 -0500

    serve media files

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from platzigram import views as localviews
 from posts import views as posts_views
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path("hi/<str:name>/<int:age>", localviews.hi),
     path("posts", posts_views.list_posts)
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
